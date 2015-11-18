e60309d4f676382dee232c989993e03ab68b7964
NewDirection <dkuperberg@gmail.com>
2015-11-18 21:24:35 +0200

    Group support added

--- server.js
+++ server.js
@@ -1,21 +1,3 @@
-//var express = require('express');
-//var app = express();
-
-////app.get('/', function (req, res) {
-////    res.send('Hello World!');
-////});
-//app.get('/', function (request, res) {
-//    console.log('aa');
-//    res.sendFile('C:\\AngularApp\\Groupy\\default.html');
-//});
-
-//var server = app.listen(3000, function () {
-//    var host = "127.0.0.1";
-//    var port = server.address().port;
-
-//    console.log('Example app listening at http://%s:%s', host, port);
-//});
-
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
@@ -23,47 +5,102 @@ var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
 app.get('/', function (request, res) {
-    console.log(__dirname);
     res.sendFile(__dirname + '/public/default.html');
 });
 server.listen(port, function () {
     var host = "127.0.0.1";
     console.log('Example app listening at http://%s:%s', host, port);
 });
-  
+
 // Routing
-app.use('/js',  express.static(__dirname + '/public/js'));
+app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use(express.static(__dirname + '/public'));
 //app.use(express.static(path.join(__dirname, 'public')));
 // Chatroom
 // usernames which are currently connected to the chat
-var usernames = {};
+
+var client = {
+    "username": "",
+    "groupName": "",
+    "socket": ""
+};
+var clientsArray = [];
 var numUsers = 0;
-  
+
 io.on('connection', function (socket) {
     var addedUser = false;
-    console.log('aa');
+    console.log('New client connected (id=' + socket.id + ').');
+   
+   
     // when the client emits 'new message', this listens and executes
+
+    
     socket.on('new message', function (data) {
-        // we tell the client to execute 'new message'
-        socket.broadcast.emit('new message', {
-            username: socket.username,
-            message: data,
-            timestamp: Date.now()
-        });
+        // we tell the client to execute 'new message'  
+        var a = JSON.stringify(data);
+        console.log(a);
+        debugger;
+
+        var clientNumberinArray = -1;
+        for (var i = 0; i < clientsArray.length; i++) {
+            if (clientsArray[i].username == data.username)
+                clientNumberinArray = i;
+        }
+
+        console.log("Client Number in array: " + clientNumberinArray);
+        //console.log("GroupName: " + clientsArray[clientNumberinArray].groupName);
+
+        for (var i = 0; i < clientsArray.length; i++) {
+            if (i == clientNumberinArray)
+                continue;
+
+            if (clientsArray[i].groupName == clientsArray[clientNumberinArray].groupName) {
+                clientsArray[i].socket.emit('new message', {
+                    username: clientsArray[i].username,
+                    message: data.message,
+                    timestamp: Date.now()
+                });
+            }
+        }
+
+
+
+        //clients[1].emit('new message', {
+        //    username: socket.username,
+        //    message: data,
+        //    timestamp: Date.now()
+        //});
+        //socket.broadcast.emit('new message', {
+        //    username: socket.username,
+        //    message: data,
+        //    timestamp: Date.now()
+        //});
         console.log('I sent it');
     });
-  
-    // when the client emits 'add user', this listens and executes
-    socket.on('add user', function (username) {
 
-        //console.log('aa');
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', function (obj) {
+        
+        console.log("add user - UserName: " + obj.username + "  " + "GroupName: " + obj.groupName);
+
 
         // we store the username in the socket session for this client
-        socket.username = username;
+        //socket.username = obj.username;
         // add the client's username to the global list
-        usernames[username] = username;
+        //usernames[obj.username] = obj.username;
+        client = {}
+        client.username = obj.username;
+        client.groupName = obj.groupName;
+        client.socket = socket;
+
+        clientsArray.push(client);
+        
+        //for (var i = 0; i < clientsArray.length; i++) {
+        //    console.log(i+": "  +" UserNane " + clientsArray[i].username+ " GroupName:  "+ clientsArray[i].groupName);
+           
+        //}
+
         ++numUsers;
         addedUser = true;
         socket.emit('login', {
@@ -75,26 +112,26 @@ io.on('connection', function (socket) {
             numUsers: numUsers
         });
     });
-  
+
     // when the client emits 'typing', we broadcast it to others
     socket.on('typing', function () {
         socket.broadcast.emit('typing', {
             username: socket.username
         });
     });
-  
+
     // when the client emits 'stop typing', we broadcast it to others
     socket.on('stop typing', function () {
         socket.broadcast.emit('stop typing', {
             username: socket.username
         });
     });
-  
+
     // when the user disconnects.. perform this
     socket.on('disconnect', function () {
         // remove the username from global usernames list
         if (addedUser) {
-            delete usernames[socket.username];
+           // delete usernames[socket.username];
             --numUsers;
 
             // echo globally that this client has left
